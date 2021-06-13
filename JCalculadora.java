87ca64cf2822f6e6b7e9e958e2b0dc91096cb498
Flávio Jr <80114913+FlavioJr70@users.noreply.github.com>
2021-06-13 20:57:45 -0300

    Add files via upload

---
+++ JCalculadora.java
@@ -0,0 +1,23 @@
+package jcalculadora;
+
+
+
+/**
+ *
+ * @author Professor
+ */
+public class JCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Super Calculadora O.O. da Etec\n"
+                         + "******************************\n");        
+        Operacoes calculos = new Operacoes();
+        calculos.menu();
+        
+    }
+    
+}
